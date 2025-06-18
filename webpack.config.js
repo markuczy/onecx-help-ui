25e4602e63deef1edb949312270f392602ea2861
Bastian Jakobi <bastian.jakobi2000@gmail.com>
2025-06-18 12:43:54 +0200

    refactor: uninstall pia and remove shared dependency

--- webpack.config.js
+++ webpack.config.js
@@ -27,10 +27,8 @@ const config = withModuleFederationPlugin({
     '@onecx/angular-remote-components': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/angular-testing': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/angular-webcomponents': { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/portal-integration-angular': { requiredVersion: 'auto', includeSecondaries: true }
-  }),
-  sharedMappings: ['@onecx/portal-integration-angular']
+    '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true }
+  })
 })
 config.devServer = { allowedHosts: 'all' }
 
